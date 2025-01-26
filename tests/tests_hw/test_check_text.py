41b184dbb4bfac1c20b1f3c1cde2d163dedca304
Vasilev_Anton <anton.vasilev91@yandex.ru>
2025-01-26 23:30:10 +0300

    Lesson 8

--- tests/tests_hw/test_check_text.py
+++ tests/tests_hw/test_check_text.py
@@ -10,7 +10,12 @@ def test_check_text_elements_page(browser):
     demo_qa_page = DemoQa(browser)
     elements_page = ElementsPage(browser)
     demo_qa_page.visit()
-    demo_qa_page.btn_elements.click()
-    assert elements_page.equal_url()
     assert elements_page.text_element.get_text() == 'Please select an item from left to start practice.'
 
+def text_page_elements(browser):
+    el_page = ElementsPage(browser)
+    el_page.visit()
+
+    assert el_page.icon.exist()
+    assert el_page.btn_sidebar_first.exist()
+    assert el_page.btn_sidebar_first_textbox.exist()
